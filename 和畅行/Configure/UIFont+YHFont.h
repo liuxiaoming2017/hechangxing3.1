54fdcd9801d0a1241735c7679f64ca3f0c3ed0e8
Wei Zhao <zw_mting@126.com>
2019-09-05 15:35:04 +0800

    no message

---
+++ 和畅行/Configure/UIFont+YHFont.h
@@ -0,0 +1,17 @@
+//
+//  UIFont+YHFont.h
+//  和畅行
+//
+//  Created by Wei Zhao on 2019/8/21.
+//  Copyright © 2019 刘晓明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIFont (YHFont)
+
+@end
+
+NS_ASSUME_NONNULL_END
