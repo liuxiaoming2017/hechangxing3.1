ff9a173469f468ffb1d742c1c90bb3d37980f541
obally <1977850379@qq.com>
2019-11-01 13:45:50 +0800

    no message

--- 和畅行/Configure/Global.h
+++ 和畅行/Configure/Global.h
@@ -23,9 +23,8 @@ extern BOOL isBF;
 
 //测试环境
 //#define URL_PRE (isProduct ? @"http://eky3h.com/healthlm/" : @"http://10.1.71.38:8888/hcy-system/")
+
 #define URL_PRE (isProduct ? @"http://eky3h.com/healthlm/" : @"http://47.92.73.99:8080/healthlm/")
-
-
 
 //预发布 http://47.92.73.99:8080/healthlm/
 
